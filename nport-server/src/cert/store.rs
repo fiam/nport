648ed511a8472d6fa60f15b8f5cd60929ae6305a
fiam <alberto@garciahierro.com>
2023-07-21 22:51:30 +0100

    fix: fallback to the default cert when there's no SNI

--- nport-server/src/cert/store.rs
+++ nport-server/src/cert/store.rs
@@ -168,18 +168,26 @@ impl Store {
 
 impl ResolvesServerCert for Store {
     fn resolve(&self, client_hello: ClientHello) -> Option<Arc<CertifiedKey>> {
+        let sni = client_hello.server_name();
+        tracing::debug!(sni, "resolve cert");
         if let Some(name) = client_hello.server_name() {
             let top_level = format!(
                 "*.{}",
                 name.split('.').skip(1).collect::<Vec<_>>().join(".")
             );
+            let certs = self.certs.read().unwrap();
+            if let Some(cert) = certs
+                .get(&top_level)
+                .or_else(|| certs.get(name))
+                .or_else(|| certs.get(&self.domain))
             {
-                let certs = self.certs.read().unwrap();
-                if let Some(cert) = certs.get(&top_level) {
-                    // TODO: Refresh if needed
-                    return Some(cert.clone());
-                }
+                tracing::trace!(cert=?cert.cert, "using cert");
+                // TODO: Refresh if needed
+                return Some(cert.clone());
             }
+            tracing::warn!(name, "no appropriate cert found");
+        } else {
+            tracing::warn!("hello without name");
         }
         None
     }
