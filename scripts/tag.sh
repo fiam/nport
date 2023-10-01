485391462aba2562c796749b75a817787eb25505
fiam <alberto@garciahierro.com>
2023-10-01 10:43:14 +0100

    chore: add script for tagging releases

---
+++ scripts/tag.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Use cargo install toml-cli to install toml
+
+version=$1
+
+CARGO_TOML_FILES="libnp/Cargo.toml np/Cargo.toml nport-server/Cargo.toml"
+for file in $CARGO_TOML_FILES; do
+    contents=$(toml set $file package.version $version)
+    echo "$contents" > $file
+done
+
+git add ${CARGO_TOML_FILES}
+git tag -s v$version -m "release: version $version"
